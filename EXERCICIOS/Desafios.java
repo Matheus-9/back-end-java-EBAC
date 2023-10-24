3e2f921d6255a0821a046ce3fa89513fc76dcb8e
Matheus-9 <matheusexu@gmail.com>
2023-10-24 07:19:58 -0300

    Adicionando melhorias ao primeiro exercicio

--- EXERCICIOS/Desafios.java
+++ EXERCICIOS/Desafios.java
@@ -4,16 +4,25 @@ public class Desafios {
         CalculoDeNotas();
     }
 
-//FAZER A LEITURA DE 4 NOTAS E DIVIDIR POR 4
-    //DESAFIO NUMERO: 01
+/*FAZER A LEITURA DE 4 NOTAS E DIVIDIR POR 4
+    DESAFIO NUMERO: 01
+*/
     public static void CalculoDeNotas(){
     int primeiroSemestre = 9;
     int segundoSemestre = 8;
     int terceiroSemestre = 6;
     int quartoSemestre = 9;
 
+    System.out.println(" *** Notas Semestrais *** ");
+    
     int resultadoFinal = (primeiroSemestre + segundoSemestre + terceiroSemestre + quartoSemestre) /4;
-    System.out.println(resultadoFinal);
+
+    if (resultadoFinal >= 7) {
+            System.out.println("Parabens você foi aprovado : "  + resultadoFinal);
+
+    } else {
+        System.out.println("Sua nota esta a baixo da media : " + resultadoFinal);
+    }
 
     }
     
