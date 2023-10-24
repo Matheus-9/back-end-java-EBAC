72d912d9cbb5f475b20ae5db0b89dd4c7f4cefdb
Matheus-9 <matheusexu@gmail.com>
2023-10-24 07:10:29 -0300

    Organizando as pastas para melhor organização

--- EXERCICIOS/Desafios.java
+++ EXERCICIOS/Desafios.java
@@ -1,14 +1,12 @@
-//FAZER A LEITURA DE 4 NOTAS E DIVIDIR POR 4
 public class Desafios {
 
     public static void main(String args[]){
         CalculoDeNotas();
     }
 
-
+//FAZER A LEITURA DE 4 NOTAS E DIVIDIR POR 4
     //DESAFIO NUMERO: 01
     public static void CalculoDeNotas(){
-        
     int primeiroSemestre = 9;
     int segundoSemestre = 8;
     int terceiroSemestre = 6;
