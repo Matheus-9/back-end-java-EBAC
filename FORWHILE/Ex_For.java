d04651709405e4a2c71807877889d62da87da0ed
Matheus-9 <matheusexu@gmail.com>
2023-11-19 08:34:21 -0300

    praticano for com for e while com tabuada

--- FORWHILE/Ex_For.java
+++ FORWHILE/Ex_For.java
@@ -1,21 +1,30 @@
 package FORWHILE;
 
 import javax.print.attribute.standard.Sides;
+import java.util.Scanner;
 
 public class Ex_For {
 
     public static void main(String asrgs[]){
-        Exemplo1();
-    }
+        Scanner scanner = new Scanner(System.in);
 
-    //Exeplo de for:
-    public static void Exemplo1(){
-        int contado;
+        System.out.println("Deseja gerar a tabuada de algum número ? (sim ou não ): ");
+        String resposta = scanner.next();
 
-        for(contado = 0; contado < 10; contado++){
-            System.out.println("Linha: " + contado);
+        while (resposta.equalsIgnoreCase("sim")){
 
+            System.out.println("Digite o numero para gerar a tabuada: ");
+            int num = scanner.nextInt();
+            for (int i = 0; i <= 10; i++){
+                System.out.println(num + " X " + i + " = " + num*i);
+            }
+            System.out.println("Deseja gera mais alguma tabuada? ");
+            resposta = scanner.next();
         }
-
+        System.out.println("Obrigado por participar !");
     }
+
+
+
+
 }
